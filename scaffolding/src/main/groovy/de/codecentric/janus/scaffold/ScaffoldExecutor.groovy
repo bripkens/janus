a16e7adb7c2bd1f97438916ffbaddf7c85ed685f
Ben Ripkens <bripkens.dev@gmail.com>
2012-02-06 12:13:26 +0100

    Added support for 'package wrapping', i.e., wrapping source code in packages is supported. Also improved the testability.

--- scaffolding/src/main/groovy/de/codecentric/janus/scaffold/ScaffoldExecutor.groovy
+++ scaffolding/src/main/groovy/de/codecentric/janus/scaffold/ScaffoldExecutor.groovy
@@ -8,17 +8,44 @@ import org.apache.commons.io.IOUtils
  * @author Ben Ripkens <bripkens.dev@gmail.com>
  */
 class ScaffoldExecutor {
-    final Scaffold scaffold
+    static final PACKAGE_WRAPPER_PLACEHOLDER = '-to_package-'
+    static final SOURCE_DIRECTORY = 'sources/'
+    static final PACKAGE_REGEX = /^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/
 
-    ScaffoldExecutor(Scaffold scaffold) {
+    final Scaffold scaffold
+    final String pckg
+    final File scaffoldFile
+
+    /**
+     * Initialize this scaffold executor using the given parameters. Instances
+     * of this class can be reused.
+     *
+     * @param scaffold The scaffold type
+     * @param pckg The package under which the target sources should be located
+     */
+    ScaffoldExecutor(Scaffold scaffold, String pckg) {
+        this(scaffold, pckg, { ScaffoldLoader.load(it) })
+    }
+
+    ScaffoldExecutor(Scaffold scaffold, String pckg, Closure scaffoldLoader) {
+        assert scaffold != null && pckg != null && scaffoldLoader != null
+
         this.scaffold = scaffold
+
+        pckg = pckg.trim()
+        if (pckg.isEmpty()) {
+            this.pckg = ''
+        } else if (pckg =~ PACKAGE_REGEX) {
+            this.pckg = pckg.replace('.', '/')
+        } else {
+            throw new ScaffoldingException('Invalid package name.')
+        }
+
+        scaffoldFile = scaffoldLoader(scaffold)
     }
 
     void apply(File targetDirectory) {
-        String home = System.getProperty('user.home')
-        String uri = home + File.separator + Catalog.SCAFFOLD_DIR +
-                File.separator + scaffold.filename
-        ZipFile file = new ZipFile(new File(uri), ZipFile.OPEN_READ)
+        ZipFile file = new ZipFile(scaffoldFile, ZipFile.OPEN_READ)
 
         Enumeration entries = file.entries()
         while(entries.hasMoreElements()) {
@@ -29,6 +56,10 @@ class ScaffoldExecutor {
     }
 
     private void unzip(ZipFile file, ZipEntry entry, File targetDirectory) {
+        if (!entry.name.startsWith(SOURCE_DIRECTORY)) {
+            return
+        }
+
         if (entry.directory) {
             unzipDirectory(entry, targetDirectory)
         } else {
@@ -37,23 +68,23 @@ class ScaffoldExecutor {
     }
 
     private void unzipDirectory(ZipEntry entry, File targetDirectory) {
-        unzipDirectory(new File(targetDirectory, entry.name))
+        createDirectory(new File(targetDirectory, updateName(entry.name)))
     }
 
-    private void unzipDirectory(File dir) {
-        if (!dir.mkdirs()) {
+    private void createDirectory(File dir) {
+        if (!dir.exists() && !dir.mkdirs()) {
             throw new ScaffoldingException("Couldn't create directory ${dir.absolutePath}")
         }
     }
 
     private void unzipFile(ZipFile zipFile, ZipEntry entry,
                            File targetDirectory) {
-        File outputFile = new File(targetDirectory, entry.getName())
+        File outputFile = new File(targetDirectory, updateName(entry.name))
 
         // ensure the parent directory exists
-        File parent = outputFile.getParentFile()
+        File parent = outputFile.parentFile
         if (!parent.exists()) {
-            unzipDirectory(parent)
+            createDirectory(parent)
         }
 
         // extract file
@@ -66,5 +97,18 @@ class ScaffoldExecutor {
             IOUtils.closeQuietly(input)
             IOUtils.closeQuietly(output)
         }
+    }
+
+    /**
+     * Transform the file name as it exists in the ZIP to an appropriate one
+     * with package hierarchy. Also, remove the parent directory "sources"
+     * which is not extracted.
+     *
+     * @param originalName Name of the ZIP file entry
+     * @return The new name which can be used for extraction
+     */
+    private String updateName(String originalName) {
+        return originalName.replace(PACKAGE_WRAPPER_PLACEHOLDER, pckg)
+                .replace(SOURCE_DIRECTORY, '')
     }
 }
--- scaffolding/src/test/groovy/de/codecentric/janus/scaffold/ScaffoldExecutorTest.groovy
+++ scaffolding/src/test/groovy/de/codecentric/janus/scaffold/ScaffoldExecutorTest.groovy
@@ -4,12 +4,25 @@ import org.junit.Before
 import org.junit.Test
 
 /**
+ * Please manually verify the generated output.
+ *
  * @author Ben Ripkens <bripkens.dev@gmail.com>
  */
 class ScaffoldExecutorTest {
 
-    @Test void testApply() {
-        ScaffoldExecutor executor = new ScaffoldExecutor(new Scaffold([filename: 'quickstart.zip']))
-        executor.apply(new File('/home/ben/tmp'))
+    Scaffold scaffold
+
+    ScaffoldExecutorTest() {
+        this.scaffold = new Scaffold([filename: 'quickstart.zip'])
+    }
+
+    @Test void testNoPackage() {
+        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold, "")
+        executor.apply(new File('/home/ben/tmp/noPackage'))
+    }
+
+    @Test void testWithPackage() {
+        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold, "de.codecentric")
+        executor.apply(new File('/home/ben/tmp/withPackage'))
     }
 }
