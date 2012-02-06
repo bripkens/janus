4f54039c08e7259b3825a9156b97ca0684016bd5
Ben Ripkens <bripkens.dev@gmail.com>
2012-02-06 13:25:24 +0100

    The scaffold tests can now directly be executed without any changes.

--- scaffolding/src/test/groovy/de/codecentric/janus/scaffold/ScaffoldExecutorTest.groovy
+++ scaffolding/src/test/groovy/de/codecentric/janus/scaffold/ScaffoldExecutorTest.groovy
@@ -2,6 +2,8 @@ package de.codecentric.janus.scaffold
 
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 /**
  * Please manually verify the generated output.
@@ -9,7 +11,6 @@ import org.junit.Test
  * @author Ben Ripkens <bripkens.dev@gmail.com>
  */
 class ScaffoldExecutorTest {
-
     Scaffold scaffold
 
     ScaffoldExecutorTest() {
@@ -17,12 +18,30 @@ class ScaffoldExecutorTest {
     }
 
     @Test void testNoPackage() {
-        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold, "")
-        executor.apply(new File('/home/ben/tmp/noPackage'))
+        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold, "",
+                { testScaffoldLoader(it) })
+        File out = getOutputDirectory('noPackage')
+        executor.apply(out)
+        println "Please verify the test results manually by" +
+                " investigating ${out.absolutePath}."
     }
 
     @Test void testWithPackage() {
-        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold, "de.codecentric")
-        executor.apply(new File('/home/ben/tmp/withPackage'))
+        ScaffoldExecutor executor = new ScaffoldExecutor(scaffold,
+                "de.codecentric", { testScaffoldLoader(it) })
+        File out = getOutputDirectory('withPackage')
+        executor.apply(out)
+        println "Please verify the test results manually by" +
+                " investigating ${out.absolutePath}."
+    }
+
+    File testScaffoldLoader(Scaffold scaffold) {
+        return new File(this.getClass().getClassLoader()
+                .getResource(scaffold.filename).toURI())
+    }
+
+    File getOutputDirectory(String subDirectory) {
+        new File(new File('.').getAbsolutePath() + File.separator +
+                'build' + File.separator + subDirectory)
     }
 }
