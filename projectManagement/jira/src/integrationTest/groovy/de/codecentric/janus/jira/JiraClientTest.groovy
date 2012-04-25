2b142aea95f771a3a9e5d93c21098063dc9f3e03
Ben Ripkens <bripkens.dev@gmail.com>
2012-04-25 14:06:59 +0200

    All basic REST web service functionality is now covered by the Jenkins component. Now some serious testing can be started.

--- projectManagement/jira/src/integrationTest/groovy/de/codecentric/janus/jira/JiraClientTest.groovy
+++ projectManagement/jira/src/integrationTest/groovy/de/codecentric/janus/jira/JiraClientTest.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 codecentric AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.codecentric.janus.jira
 
 import org.junit.Test
@@ -48,5 +64,19 @@ class JiraClientTest {
         assert groups.any { it.name == 'jira-users' }
         assert groups.any { it.name == 'jira-administrators' }
         assert groups.any { it.name == 'jira-developers' }
+    }
+
+    @Test void shouldRetrieveAllProjects() {
+        client.getProjects()
+
+        // For now this is an assertion free test. While it does not test much,
+        // it ensures that the main logic is executed properly. Assertions
+        // can not be used at this point since we do not know if projects exist
+        // or not. Further tests can be made as part of the createProject(...)
+        // tests.
+    }
+
+    @Test void shouldSearchUser() {
+        assert client.searchUser(USERNAME).any { it.name == USERNAME }
     }
 }
--- projectManagement/jira/src/main/groovy/de/codecentric/janus/jira/JiraRestClientImpl.groovy
+++ projectManagement/jira/src/main/groovy/de/codecentric/janus/jira/JiraRestClientImpl.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 codecentric AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.codecentric.janus.jira
 
 import groovyx.net.http.HTTPBuilder
@@ -12,6 +28,7 @@ import groovy.json.JsonSlurper
 import org.apache.http.HttpEntity
 import de.codecentric.janus.jira.model.RemoteGroupSummary
 import de.codecentric.janus.jira.model.RemoteProjectSummary
+import com.atlassian.jira.rpc.soap.beans.RemoteUser
 
 /**
  * @author Ben Ripkens <bripkens.dev@gmail.com>
@@ -47,7 +64,7 @@ class JiraRestClientImpl implements JiraRestClient {
 
     @Override
     RemoteProjectSummary[] getProjects() {
-        def result = null
+        def result = []
 
         get(baseUrl, PATH.GET_ALL_PROJECTS) { HttpResponse resp ->
             HttpEntity entity = resp.entity
@@ -65,11 +82,38 @@ class JiraRestClientImpl implements JiraRestClient {
         return result
     }
 
+    @Override
+    RemoteUser[] searchUser(String name) {
+        def result = []
+
+        get(baseUrl, PATH.GET_SEARCH_USER, [username: name]) { HttpResponse resp ->
+            HttpEntity entity = resp.entity
+
+            def slurper = new JsonSlurper()
+            def json = slurper.parse(entity.content.newReader('UTF-8'))
+
+            json.each {
+                result.add(new RemoteUser(it.emailAddress,
+                        it.displayName,
+                        it.name))
+            }
+
+            entity.consumeContent()
+        }
+
+        return result
+    }
+
     private get(String baseUrl, String path, Closure success) {
+        get(baseUrl, path, [] as HashMap, success)
+    }
+
+    private get(String baseUrl, String path, Map query, Closure success) {
         HTTPBuilder http = getHTTPBuilder()
 
         http.request(GET, ANY) {
             uri.path = path
+            uri.query = query
             response.success = success
             response.failure = defaultFailureHandler
         }
@@ -79,7 +123,7 @@ class JiraRestClientImpl implements JiraRestClient {
         HTTPBuilder http = new HTTPBuilder(baseUrl)
 
         // HTTP builder needs to be put in preemptive mode since requests to
-        // the JIRA REST web service may fail silently, i.e., this just
+        // the JIRA REST web service may fail silently, i.e., they just
         // produce no result. Therefore http.auth.basic(name, apiToken) isn't
         // used
         String encodedCredentials = (credentials.name +
@@ -107,5 +151,6 @@ class JiraRestClientImpl implements JiraRestClient {
         String BASE_PATH = '/rest/api/2/'
         String GET_ALL_GROUPS = BASE_PATH + 'groups/picker'
         String GET_ALL_PROJECTS = BASE_PATH + 'project'
+        String GET_SEARCH_USER = BASE_PATH + 'user/search'
     }
 }
