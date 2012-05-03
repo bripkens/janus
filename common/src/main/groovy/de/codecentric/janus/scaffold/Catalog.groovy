bc6a48c21ecd50a8ee93bdc2afd8cbe30d90824b
Ben Ripkens <bripkens.dev@gmail.com>
2012-05-03 15:46:02 +0300

    Update common/src/main/groovy/de/codecentric/janus/scaffold/Catalog.groovy

--- common/src/main/groovy/de/codecentric/janus/scaffold/Catalog.groovy
+++ common/src/main/groovy/de/codecentric/janus/scaffold/Catalog.groovy
@@ -25,7 +25,6 @@ class Catalog {
     List<CatalogEntry> scaffolds
 
     static Catalog from(File file) {
-        ObjectMapper mapper = new ObjectMapper();
         new ObjectMapper().readValue(file, Catalog.class)
     }
 }
